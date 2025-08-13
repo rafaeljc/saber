bc7bdca3e09fdf590f93a43f05e58dcf96228ea1
Rafael Clemente <rafaeljc@users.noreply.github.com>
2025-08-13 15:50:51 -0300

    Implement message history display

--- saber/chatbot.py
+++ saber/chatbot.py
@@ -14,7 +14,19 @@ class Chatbot:
             name: Defines the name of the chatbot. (Default: "S.A.B.E.R.")
         """
         self.name = name
+        if not st.session_state.get("messages"):
+            st.session_state.messages = []
+
+    def show_message_history(self):
+        """Displays the message history."""
+        if len(st.session_state.messages) == 0:
+            with st.chat_message("assistant"):
+                st.markdown("Hello! How can I assist you today?")
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
     def run(self):
         """Run the chatbot."""
         st.title(f"{self.name}")
+        self.show_message_history()
