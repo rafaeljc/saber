1a4289c08ee6a26ed9996dde3b6ec6a623a5c609
Rafael Clemente <rafaeljc@users.noreply.github.com>
2025-08-13 10:29:30 -0300

    Enable command line execution after installing with pip

--- saber/app.py
+++ saber/app.py
@@ -1,3 +1,19 @@
+import subprocess
+
+def run():
+    """Run the application from the command line.
+    
+    Enables the user to start the application by running the 'saber' command in 
+    the terminal after installing it with pip.
+
+    Usage example:
+        $ pip install .
+        $ saber
+    """
+    try:
+        subprocess.run(["python", __file__])
+    except KeyboardInterrupt:
+        print("Application stopped by user.")
 
 def main():
     print("Hello, I'm S.A.B.E.R.!")
