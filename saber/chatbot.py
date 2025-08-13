86e9f8bf5137129527dd251a840bf5f1536f19b1
Rafael Clemente <rafaeljc@users.noreply.github.com>
2025-08-13 13:50:37 -0300

    Add Chatbot class

---
+++ saber/chatbot.py
@@ -0,0 +1,19 @@
+    
+class Chatbot:
+    """Implements the chatbot user interface and functionalities
+
+    Attributes:
+        name: A string of the chatbot's name.
+    """
+
+    def __init__(self, name: str = "S.A.B.E.R."):
+        """Initializes the chatbot with a name and does the initial setup.
+        
+        Args:
+            name: Defines the name of the chatbot. (Default: "S.A.B.E.R.")
+        """
+        self.name = name
+
+    def run(self):
+        """Run the chatbot."""
+        print(f"Hello, I'm {self.name}!")
